5c30c76294212d56ad1eb8f13abd20625db89174
sakib <mohammadsakib7679@gmail.com>
2025-05-17 11:59:38 +0600

    structure routes as moduler pattern

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors';
-import { UserController } from './app/modules/User/user.controller';
+import router from './routes';
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/user/create-user', UserController.createUser)
+app.use('/api/v1', router)
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.send("Task Management Project Is Running!");
 })
 
 export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { UserRoutes } from "../app/modules/User/user.routes";
+
+const router = Router();
+
+
+const moduleRoutes = [
+    {
+        path: '/user',
+        route: UserRoutes
+    },
+    
+    
+]
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
